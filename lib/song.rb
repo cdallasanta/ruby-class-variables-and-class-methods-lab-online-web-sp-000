e4ee4c499cf020127aabd3777df7e99b6882a09f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-03 04:24:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :genre, :artist
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    count = {}
+    self.genres.each do |key|
+      count[key] = @@genres.count(key)
+    end
+    count
+  end
+
+  def self.artist_count
+
+  end
+
+end
